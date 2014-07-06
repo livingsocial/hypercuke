8a2b0168eee603a92b9956f4c15bb999f8fffbe4
Sam Livingston-Gray <sam.livingstongray@livingsocial.com>
2014-07-05 21:19:11 -1000

    Explicitly disable RSpec’s “.should” syntax

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,7 @@
 require_relative '../lib/hypercuke'
 
+RSpec.configure do |config|
+  config.expect_with :rspec do |c|
+    c.syntax = :expect # I hate Kernel#should with the fire of a thousand suns
+  end
+end
